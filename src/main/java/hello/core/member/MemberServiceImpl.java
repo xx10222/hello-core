98cd441c0fbf495d9d4bce241fb1d16d18f5431d
xx10222 <yjw4960@gmail.com>
2022-10-15 01:16:17 +0900

    :sparkles: 회원 도메인

---
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -0,0 +1,16 @@
+package hello.core.member;
+
+public class MemberServiceImpl implements MemberService {
+    // 인터페이스만 선언해두면 NullPointerException 이 발생하기 때문에 구현체를 선택해야 한다
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    @Override
+    public void join(Member member) {
+        memberRepository.save(member);
+    }
+
+    @Override
+    public Member findMember(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
---
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -0,0 +1,20 @@
+package hello.core.member;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryMemberRepository implements MemberRepository {
+
+    // HashMap은 동시성 이슈가 발생할 수 있어서 실무에서는 ConcurrentHashMap을 사용한다
+    private static Map<Long, Member> store = new HashMap<>();
+
+    @Override
+    public void save(Member member) {
+        store.put(member.getId(), member);
+    }
+
+    @Override
+    public Member findById(Long memberId) {
+        return store.get(memberId);
+    }
+}
