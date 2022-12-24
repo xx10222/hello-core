d684574f30a59085c9e7c265a1feb9422e6f8129
xx10222 <yjw4960@gmail.com>
2022-12-24 18:27:24 +0900

    :sparkles: 스프링 빈 조회 테스트 - 상속 관계

--- src/test/java/hello/core/beanfind/ApplicationContextExtendsFindTest.java
+++ src/test/java/hello/core/beanfind/ApplicationContextExtendsFindTest.java
@@ -1,6 +1,5 @@
 package hello.core.beanfind;
 
-import hello.core.AppConfig;
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
